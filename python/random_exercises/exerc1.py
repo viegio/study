9a3e0f9f5370291917099e5b9719713af478bf8d
Giovanna Viecelli <105893274+viegio@users.noreply.github.com>
2022-06-29 20:32:29 -0300

    Create exerc1.py

---
+++ python/random_exercises/exerc1.py
@@ -0,0 +1,4 @@
+# Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado
+
+l = int(input('Informe o valor de um dos lados do quadrado: '))
+print('A área do quadrado é: ' + str(l*l))
